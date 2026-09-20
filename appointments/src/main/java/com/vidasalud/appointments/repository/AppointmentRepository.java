ac668e11bb3d751df2316648022ea8370c52c0ca
MiguelAllende01 <migu.allende@duocuc.cl>
2026-09-20 17:15:37 -0300

    cambios

--- appointments/src/main/java/com/vidasalud/appointments/repository/AppointmentRepository.java
+++ appointments/src/main/java/com/vidasalud/appointments/repository/AppointmentRepository.java
@@ -1,7 +1,6 @@
 package com.vidasalud.appointments.repository;
 
 import com.vidasalud.appointments.model.Appointment;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
